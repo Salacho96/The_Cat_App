4a2aa9501be4362c4044d3736b23e33fdec49e95
dbook-1629 <dbook-1629@outlook.com>
2022-11-10 22:03:52 -0500

    Initial Commit

---
+++ cat_app/cat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cat_appApp.swift
+//  cat_app
+//
+//  Created by Andres Salazar on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct cat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
